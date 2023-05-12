fa5e912ec119a3284cf6a3bb0f60765eb53c3b8b
= <inman.turbo@gmail.com>
2023-05-12 01:09:10 -0400

    update config

--- config/cloudflare-dns.php
+++ config/cloudflare-dns.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    // Here you can define the schedule for checking if dns records should be updated. The default is every 8 minutes.
+    // Here you can define the schedule for checking if dns records should be updated. The default is every 8 minutes ('*/8 * * * *').
     // see https://laravel.com/docs/8.x/scheduling#schedule-frequency-options for more information
     'schedule' => '*/8 * * * *',
 ];
